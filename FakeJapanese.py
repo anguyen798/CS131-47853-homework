ffe95f56ebf83efbcfa98be6709ac4e6b1567103
Nguyen <anguyen798@student.cccd.edu>
2023-02-01 20:01:24 -0800

    Files for day 2

---
+++ FakeJapanese.py
@@ -0,0 +1,11 @@
+hello_world = "Ohayoo Goyazmashtah"
+# This is Psuedojapnesh
+def FakeJapanese():
+    print(hello_world)
+    print("What is 1+2+3+4+5+6+7+8+9")
+    print ("I think the answer is 45")
+    input("Press enter to find out the answer;")
+    print(1+2+3+4+5+6+7+8+9)
+    print (1/0)
+''' My first comment block '''
+FakeJapanese()
