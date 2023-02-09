f4edffd77c18fdc88b258c38629280ee69e8cdab
Nguyen <anguyen798@student.cccd.edu>
2023-02-08 20:28:47 -0800

    Temporary WIP Code

---
+++ Chapter 2/nguyen_albert_Lab_2.py
@@ -0,0 +1,70 @@
+# Lab 2a
+"""
+Soft drinks are sold in cans and bottles. A store offers a six-pack of
+12-ounce cans for the same price as a two-liter bottle. Which should
+you buy? (1 liter is 33.814 ounces)
+
+You will need the following variables:
+List of variables                                    Type of Number
+Number of cans per pack                         Whole number
+Ounces per can                                        Whole number
+Ounces per bottle                                     Number with fraction
+
+Write a program that calculates the capacity of soda in a six-pack of soda and print it.
+
+Print a statement at the end that states which is the better buy. Your program should demonstrate the use of variables and constants.
+"""
+
+CANS_PER_PACK = 6
+OUNCES_PER_CAN = 12
+OUNCES_PER_BOTTLE = 
+
+
+
+# Lab 2b 
+"""
+Given two integers, write a program that prints
+The sum
+The difference
+The product
+The average
+The distance (absolute value of the difference)
+The maximum (the larger of the two)
+The minimum (the smaller of the two)
+Hint: Python defines max and min functions that accept a sequence of values, each
+separated with a comma.
+
+Test your program with the following integers:
+3, 8
+-2, 0
+-5, -1
+-4, 4
+"""
+
+def lab2b (x,y):
+    lab2bSum = sum(x,
+    lab2bDifference = difference (x - y 
+    lab2bProduct =
+    lab2bAverage =
+    lab2bDistance =
+    lab2bMaximum = 
+    lab2bMinimum =
+    return lab2bSum, lab2bDifference, lab2bProduct, lab2bAverage, lab2bDistance, lab2bMaximum, lab2bMinimum    
+print(
+
+
+
+
+# Lab 2c
+"""
+Find and print the result of the following 3 equations:
+x=10   y = 6     a = 2    b= 8    c = 1
+"""
+import math
+x, y, a, b, c = 10, 6, 2, 8, 1
+lab2cSqrt = math.sqrt(x+y)
+lab2cQuadratic = -b + math.sqrt(b ** 2 - 4 * a * c)/(2 * a)
+lab2cExponent = x ** (y+7)
+print(lab2cSqrt)
+print(lab2cQuadratic)
+print(lab2cExponent)
---
+++ Chapter 2/roughdraft.py
@@ -0,0 +1,56 @@
+"""
+3, 8
+-2, 0
+-5, -1
+-4, 4
+"""
+x, y = 3, 8
+
+LAB_2B_SUM = x + y
+LAB_2B_DIFFERENCE = x - y
+LAB_2B_PRODUCT = x * y
+LAB_2B_AVERAGE = (x + y)/2
+LAB_2B_DISTANCE = abs(x - y)
+LAB_2B_MAXIMUM = max(x, y)
+LAB_2B_MINIMUM = min (x,y)
+
+
+x = 3
+y = 8
+print("Sum is: ", LAB_2B_SUM)
+print("Difference is:", LAB_2B_DIFFERENCE)
+print("Product is :", LAB_2B_PRODUCT)
+print("Average is: ", LAB_2B_AVERAGE)
+print("Distance is: ", LAB_2B_DISTANCE)
+print("Maximum is: ", LAB_2B_MAXIMUM)
+print("Minimum is: ", LAB_2B_MINIMUM)
+
+x = -2
+y = 0
+print("Sum is: ", LAB_2B_SUM)
+print("Difference is:", LAB_2B_DIFFERENCE)
+print("Product is :", LAB_2B_PRODUCT)
+print("Average is: ", LAB_2B_AVERAGE)
+print("Distance is: ", LAB_2B_DISTANCE)
+print("Maximum is: ", LAB_2B_MAXIMUM)
+print("Minimum is: ", LAB_2B_MINIMUM)
+
+x = -5
+y = -1
+print("Sum is: ", LAB_2B_SUM)
+print("Difference is:", LAB_2B_DIFFERENCE)
+print("Product is :", LAB_2B_PRODUCT)
+print("Average is: ", LAB_2B_AVERAGE)
+print("Distance is: ", LAB_2B_DISTANCE)
+print("Maximum is: ", LAB_2B_MAXIMUM)
+print("Minimum is: ", LAB_2B_MINIMUM)
+
+x = -4
+y = 4
+print("Sum is: ", LAB_2B_SUM)
+print("Difference is:", LAB_2B_DIFFERENCE)
+print("Product is :", LAB_2B_PRODUCT)
+print("Average is: ", LAB_2B_AVERAGE)
+print("Distance is: ", LAB_2B_DISTANCE)
+print("Maximum is: ", LAB_2B_MAXIMUM)
+print("Minimum is: ", LAB_2B_MINIMUM)
