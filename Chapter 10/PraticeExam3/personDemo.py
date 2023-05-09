29a5b4ec2e216aae27a1759fda0f10f223c73b70
anguyen798 <anguyen798@student.cccd.edu>
2023-05-08 20:05:02 -0700

    Method Overriding

--- Chapter 10/PraticeExam3/personDemo.py
+++ Chapter 10/PraticeExam3/personDemo.py
@@ -1,12 +1,13 @@
 from persons import *
 
 def main() :
+    person = Person("Good Example", 2022)
     student = Student("Susana Frank", 2000, "CS")
-    print("%s, born in %d is a %s major"
-          % (student.get_name(), student.get_year(), student.get_major()))
     instructor = Instructor("Malin Andrew", 1979, 75000)
-    print("%s, born in %d has a %.2f salary"
-          % (instructor.get_name(), instructor.get_year(), instructor.get_salary()))
+    
+    print(person.getInfo())
+    print(student.getInfo())
+    print(instructor.getInfo())
 
     
 main()
--- Chapter 10/PraticeExam3/persons.py
+++ Chapter 10/PraticeExam3/persons.py
@@ -3,11 +3,10 @@ class Person :
         self._name = name
         self._year = year
         
-    def get_name(self) :
-        return self._name
-    
-    def get_year(self) :
-        return self._year
+# Used method overwriting instead
+
+    def getInfo(self) :
+        return "%s, born in %d" % (self._name, self._year)
 
 class Student(Person) :
     def __init__(self, name, year, major) :
@@ -23,8 +22,11 @@ class Student(Person) :
     #def get_year(self) :
         #return super().get_year()
     
-    def get_major(self) :
-        return self._major
+    #def get_major(self) :
+        #return self._major
+
+    def getInfo(self) :
+        return "%s, born in %d is a %s major" % (self._name, self._year, self._major)
 
 class Instructor(Person) :
     def __init__(self, name, year, salary) :
@@ -40,5 +42,9 @@ class Instructor(Person) :
     #def get_year(self) :
         #return super().get_year()
 
-    def get_salary(self) :
-        return self._salary
+    #def get_salary(self) :
+        #return self._salary
+    
+    def getInfo(self) :
+        return "%s, born in %d has a %.2f salary" % (self._name, self._year, self._salary)
+    
